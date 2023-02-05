bbb06e00b90a3bb034938d03f5a537248424c14c
Phoebe <Phoebe@konshin>
2023-02-05 06:16:06 +0100

    fix slugs build

---
+++ src/routes/billing/payment/method/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Payment Method</title>
+	<meta name="description" content="Payment Method" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Payment Method TODO</h1>
+</div>
